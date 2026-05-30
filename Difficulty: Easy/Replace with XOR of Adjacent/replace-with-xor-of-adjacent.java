e597841c3dd20c5cfb7b0cef175c9d419335c964
Bitu Kumar <152399673+bitu-kumar1269@users.noreply.github.com>
2026-05-30 10:00:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Replace with XOR of Adjacent/replace-with-xor-of-adjacent.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void replaceElements(int[] arr) {
+        int n = arr.length;
+        if (n < 2) return;
+
+        int prev = arr[0];
+
+        arr[0] = arr[0] ^ arr[1];
+
+        for (int i = 1; i < n - 1; i++) {
+            int currentOriginal = arr[i]; 
+            arr[i] = prev ^ arr[i + 1];   
+            prev = currentOriginal;      
+        }
+
+        arr[n - 1] = prev ^ arr[n - 1];
+    }
+}
